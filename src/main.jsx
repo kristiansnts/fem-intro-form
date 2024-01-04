8c7378368ed8544e6b793cf4011c4cfe3da91d55
kristiansnts <57317793+kristiansnts@users.noreply.github.com>
2024-01-04 13:28:24 +0700

    mobile styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './global.css'
 import Home from './pages/Home/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,35 +2,57 @@ import './Home.module.css'
 
 const Home = () => {
   return (
-    <main>
-      <div className="intro">
-        <h1>Learn to code by watching others</h1>
-        <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
-  but understanding how developers think is invaluable. </p>
-      </div>
-      <div className="form__action">
-        <button>
-          <span><strong>Try it free 7 days </strong>then $20/mo. thereafter</span>
-        </button>
-        <form>
-          <div className="form__group">
-            <input type="text" value="First Name"/>
-          </div>
-          <div className="form__group">
-            <input type="text" value="Last Name"/>
-          </div>
-          <div className="form__group">
-            <input type="text" value="Email Address"/>
-          </div>
-          <div className="form__group">
-            <input type="text" value="Password"/>
-          </div>
-          <button type="submit">Claim your free trial</button>
-          <span className="agreement">
-            By clicking the button, you are agreeing to our <a href="">Terms and Services</a>
-          </span>
-        </form>
-      </div>
+    <main className="flex h-screen items-center justify-center">
+      <section className='w-4/5 sm:flex'>
+        <div className="intro text-center text-white mb-8">
+          <h1 className='text-3xl font-bold mb-5'>Learn to code by watching others</h1>
+          <p className='text-md'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+            but understanding how developers think is invaluable. </p>
+        </div>
+        <div className="form__action">
+          <button className='w-full my-3 p-4 bg-accent-blue rounded-md shadow-md'>
+            <span className='block w-3/5 mx-auto text-white text-xs font-normal'><strong className='font-bold'>Try it free 7 days </strong>then $20/mo. thereafter</span>
+          </button>
+          <form className='p-5 my-2 bg-white rounded'>
+            <div className="form__group">
+              <input 
+                type="text" 
+                value="First Name"
+                className='w-full p-3 mb-3 text-sm text-slate-600 font-medium border rounded '
+              />
+            </div>
+            <div className="form__group">
+              <input 
+                type="text" 
+                value="Last Name"
+                className='w-full p-3 mb-3 text-sm text-slate-600 font-medium border rounded '
+              />
+            </div>
+            <div className="form__group">
+              <input 
+                type="text" 
+                value="Email Address"
+                className='w-full p-3 mb-3 text-sm text-slate-600 font-medium border rounded '
+              />
+            </div>
+            <div className="form__group">
+              <input 
+                type="text" 
+                value="Password"
+                className='w-full p-3 mb-3 text-sm text-slate-600 font-medium border rounded '
+              />
+            </div>
+            <button 
+              type="submit" 
+              className='w-full p-3 bg-primary-green text-white font-medium uppercase shadow-inner rounded'  
+            >Claim your free trial</button>
+            <span className="block text-sm text-center mt-3 w-5/6 mx-auto text-slate-600">
+              By clicking the button, you are agreeing to our 
+              <a href="" className='text-primary-red'> Terms and Services</a>
+            </span>
+          </form>
+        </div>
+      </section>
     </main>
   )
 }
