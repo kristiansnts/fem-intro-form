707daa4eb1a41ef0bdd77ce96b2025c056c364d1
kristiansnts <57317793+kristiansnts@users.noreply.github.com>
2024-01-04 13:08:34 +0700

    adding html

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,36 @@ import './Home.module.css'
 
 const Home = () => {
   return (
-    <div className='text-3xl'>Hello World</div>
+    <main>
+      <div className="intro">
+        <h1>Learn to code by watching others</h1>
+        <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+  but understanding how developers think is invaluable. </p>
+      </div>
+      <div className="form__action">
+        <button>
+          <span><strong>Try it free 7 days </strong>then $20/mo. thereafter</span>
+        </button>
+        <form>
+          <div className="form__group">
+            <input type="text" value="First Name"/>
+          </div>
+          <div className="form__group">
+            <input type="text" value="Last Name"/>
+          </div>
+          <div className="form__group">
+            <input type="text" value="Email Address"/>
+          </div>
+          <div className="form__group">
+            <input type="text" value="Password"/>
+          </div>
+          <button type="submit">Claim your free trial</button>
+          <span className="agreement">
+            By clicking the button, you are agreeing to our <a href="">Terms and Services</a>
+          </span>
+        </form>
+      </div>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      'primary-red': '#ff7a7a',
+      'primary-green': '#38cc8c',
+      'accent-blue': '#6055a5',
+      'dark-blue': '#3e3c49',
+      'gray-blue': '#b9b6d3'
+    }
   },
   plugins: [],
 }
