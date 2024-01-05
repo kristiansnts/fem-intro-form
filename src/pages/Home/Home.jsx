6d1654c0afc11b88176eef8897d6624bc0522910
kristiansnts <57317793+kristiansnts@users.noreply.github.com>
2024-01-05 12:52:50 +0700

    fix responsive problem background

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,11 @@
 import './Home.module.css'
 
 const Home = () => {
+
   return (
     <main className="flex h-screen items-center justify-center">
       <section className='w-4/5 lg:flex'>
-        <div className="intro text-center text-white mb-8 flex flex-col justify-center lg:w-1/2 lg:text-left lg:mr-20">
-          <h1 className='text-3xl font-bold mb-5'>Learn to code by watching others</h1>
-          <p className='text-sm'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
-            but understanding how developers think is invaluable. </p>
-        </div>
+        <Intro />
         <div className="form__action">
           <button className='w-full my-3 p-4 bg-accent-blue rounded-md shadow-md'>
             <span className='block w-3/5 mx-auto text-white text-xs font-normal'><strong className='font-bold'>Try it free 7 days </strong>then $20/mo. thereafter</span>
@@ -17,28 +14,28 @@ const Home = () => {
             <div className="form__group">
               <input 
                 type="text" 
-                value="First Name"
+                placeholder="First Name"
                 className='w-full p-3 mb-3 text-sm text-slate-600 font-medium border rounded '
               />
             </div>
             <div className="form__group">
               <input 
                 type="text" 
-                value="Last Name"
+                placeholder="Last Name"
                 className='w-full p-3 mb-3 text-sm text-slate-600 font-medium border rounded '
               />
             </div>
             <div className="form__group">
               <input 
                 type="text" 
-                value="Email Address"
+                placeholder="Email Address"
                 className='w-full p-3 mb-3 text-sm text-slate-600 font-medium border rounded '
               />
             </div>
             <div className="form__group">
               <input 
                 type="text" 
-                value="Password"
+                placeholder="Password"
                 className='w-full p-3 mb-3 text-sm text-slate-600 font-medium border rounded '
               />
             </div>
@@ -54,6 +51,16 @@ const Home = () => {
         </div>
       </section>
     </main>
+  )
+}
+
+const Intro = () => {
+  return (
+    <div className="intro text-center text-white mb-8 flex flex-col justify-center lg:w-1/2 lg:text-left lg:mr-20">
+          <h1 className='text-3xl font-bold mb-5'>Learn to code by watching others</h1>
+          <p className='text-sm'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+            but understanding how developers think is invaluable. </p>
+        </div>
   )
 }
 
